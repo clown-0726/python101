d66b0b9eaa975de790af77278f41e79a4d3f0ab3
Lilu Cao <lilu.cao@orbitfin.ai>
2022-10-15 20:31:44 +0800

    delete previous files

--- djangorest/server/apps/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from user import views
-
-urlpatterns = [
-    url(r'^auth_test$', views.AuthTestView.as_view(), name="Auth Test"),
-    # url(r'^info$', UserInfo.as_view(), name="USERINFO"),
-]
