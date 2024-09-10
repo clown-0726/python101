abece02084aa5fdc6fcf60cb8f3be192b4535e2c
Lilu Cao <lilu.cao@orbitfin.ai>
2024-09-10 19:30:37 +0800

    move cloud related files

--- cloud/aws/s3_stream_read_txt_by_line.py
+++ cloud/aws/s3_stream_read_txt_by_line.py
@@ -11,6 +11,7 @@ file_key = 'txt-vector/reports-data/stock_gr/2022/09/30/4d6f9c5e-83ec-3ba6-aae1-
 response = s3.get_object(Bucket=bucket_name, Key=file_key)
 streaming_body = response['Body']
 
+# 建立一个基于流读取的迭代器
 for line in streaming_body.iter_lines():
     if line:
         # 处理每一行数据
