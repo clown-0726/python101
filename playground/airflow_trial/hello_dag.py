49fa90fa672cf0a0743d4da108bde43d515c41c8
Lilu Cao <lilu.cao@orbitfin.ai>
2024-09-10 19:33:09 +0800

    Add hello airflow

---
+++ playground/airflow_trial/hello_dag.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',  # 所属用户
+    'start_date': datetime(2024, 9, 8),  # 第一次执行的时间
+    'retries': 1,  # 失败重试次数
+    'retry_delay': timedelta(minutes=3)  # 失败重试间隔
+}
+
+dag = DAG(
+    # 指定 DAG 的 ID，这个会显示到 UI 上
+    dag_id="my_workflow_execute_shell",
+    # 默认参数
+    default_args=default_args,
+    # DAG 的调度周期
+    schedule_interval=timedelta(days=1),
+)
+
+# 定义 task
+bash_task_1 = BashOperator(
+    task_id="bash_task_1",
+    bash_command='echo "Hello bash_task_1!"',
+    dag=dag,
+)
+
+bash_task_2 = BashOperator(
+    task_id="bash_task_2",
+    bash_command='echo "Hello bash_task_2!"',
+    dag=dag,
+)
+
+bash_task_3 = BashOperator(
+    task_id="bash_task_3",
+    bash_command='echo "Hello bash_task_3!"',
+    dag=dag,
+)
+
+# 建立任务之间的执行关系
+bash_task_1 >> bash_task_2 >> bash_task_3
+
+# 也可以通过下面 API 的方式设置（推荐使用上面的方式）
+# bash_task_2.set_upstream(bash_task_1)
+# bash_task_2.set_downstream(bash_task_3)
