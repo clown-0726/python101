8077dff3c643e0df5a983c0eecaf6c650c545462
Lilu <lilu.cao@orbitfin.ai>
2025-02-27 11:34:03 +0800

    add pytesseract demo

---
+++ playground/image_recognize/jdyzm.py
@@ -0,0 +1,41 @@
+from PIL import Image
+import pytesseract
+
+
+def binarizing(img, threshold):
+    pixdata = img.load()
+    w, h = img.size
+    for y in range(h):
+        for x in range(w):
+            if pixdata[x, y] < threshold:
+                pixdata[x, y] = 0
+            else:
+                pixdata[x, y] = 255
+    return img
+
+
+def depoint(img):
+    pixdata = img.load()
+    w, h = img.size
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            count = 0
+            if pixdata[x, y - 1] > 245:
+                count = count + 1
+            if pixdata[x, y + 1] > 245:
+                count = count + 1
+            if pixdata[x - 1, y] > 245:
+                count = count + 1
+            if pixdata[x + 1, y] > 245:
+                count = count + 1
+            if count > 2:
+                pixdata[x, y] = 255
+    return img
+
+
+if __name__ == '__main__':
+    l_img = './img/4.jpeg'
+    img = Image.open(l_img).convert("L")
+    img = binarizing(img, 120)
+    img = depoint(img)
+    print(pytesseract.image_to_string(img).strip())
