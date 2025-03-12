455292fa8df6915a2b1a4d38c754acba02f06c35
Lilu <lilu.cao@orbitfin.ai>
2025-03-12 08:58:03 +0800

    add example cloud/aws/s3/stream_read_by_line.py

--- cloud/aws/s3/stream_read_by_line.py
+++ cloud/aws/s3/stream_read_by_line.py
@@ -1,23 +1,21 @@
 import boto3
-import tracemalloc
 
-s3 = boto3.client('s3')
 
-tracemalloc.start()
+def read_by_line():
+    s3 = boto3.client('s3')
 
-bucket_name = 'filing-reports'
-file_key = 'txt-vector/reports-data/stock_gr/2022/09/30/4d6f9c5e-83ec-3ba6-aae1-e59c755d41a7.pdf/blocks.txt'
+    bucket_name = 'filing-reports'
+    file_key = 'txt-vector/reports-data/stock_gr/2022/09/30/4d6f9c5e-83ec-3ba6-aae1-e59c755d41a7.pdf/blocks.txt'
 
-response = s3.get_object(Bucket=bucket_name, Key=file_key)
-streaming_body = response['Body']
+    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+    streaming_body = response['Body']
 
-# 建立一个基于流读取的迭代器
-for line in streaming_body.iter_lines():
-    if line:
-        # 处理每一行数据
-        print(line.decode('utf-8'))
+    # 建立一个基于流读取的迭代器
+    for line in streaming_body.iter_lines():
+        if line:
+            # 处理每一行数据
+            print(line.decode('utf-8'))
 
-        current, peak = tracemalloc.get_traced_memory()
-        print(f"Current memory usage is {current / 10 ** 6}MB, Peak was {peak / 10 ** 6}MB.")
 
-tracemalloc.stop()
+if __name__ == "__main__":
+    read_by_line()
